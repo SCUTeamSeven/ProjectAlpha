8d9c76ffd88fc0b755dddf75ba1f3a50990c0d7c
csark0812 <csark0812@yahoo.com>
2021-11-04 11:16:11 -0700

    Functional google login, returns all profile details

--- mscms-frontend/src/components/GoogleSignin.vue
+++ mscms-frontend/src/components/GoogleSignin.vue
@@ -29,15 +29,20 @@ export default {
         if (!googleUser) {
           return null
         }
-        console.log('googleUser', googleUser)
-        this.user = googleUser.getBasicProfile().getEmail()
-        console.log('getId', this.user)
-        console.log('getBasicProfile', googleUser.getBasicProfile())
-        console.log('getAuthResponse', googleUser.getAuthResponse())
-        console.log(
-          'getAuthResponse',
-          this.$gAuth.instance.currentUser.get().getAuthResponse()
-        )
+        // Useful data for your client-side scripts:
+        var profile = googleUser.getBasicProfile()
+        this.user = profile.getEmail()
+        console.log(profile)
+        console.log('ID: ' + profile.getId()) // Don't send this directly to your server!
+        console.log('Full Name: ' + profile.getName())
+        console.log('Given Name: ' + profile.getGivenName())
+        console.log('Family Name: ' + profile.getFamilyName())
+        console.log('Image URL: ' + profile.getImageUrl())
+        console.log('Email: ' + profile.getEmail())
+
+        // The ID token you need to pass to your backend:
+        var idToken = googleUser.getAuthResponse().id_token
+        console.log('ID Token: ' + idToken)
       } catch (error) {
         // on fail do something
         console.error(error)
--- mscms-frontend/src/main.js
+++ mscms-frontend/src/main.js
@@ -3,5 +3,5 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import gAuth from 'vue3-google-oauth2'
-const gAuthOptions = { clientId: '51950083564-a1pe4gencpmiqk22rb0osfci2f944f27.apps.googleusercontent.com', scope: 'email', prompt: 'consent', fetch_basic_profile: false }
+const gAuthOptions = { clientId: '51950083564-a1pe4gencpmiqk22rb0osfci2f944f27.apps.googleusercontent.com', scope: 'profile email', prompt: 'consent', fetch_basic_profile: true }
 createApp(App).use(gAuth, gAuthOptions).use(store).use(router).mount('#app')
