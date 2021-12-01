735d1234ffb108552af66dfaa7fbfb853052d359
Axel Hernandez <axel.hernandez429@gmail.com>
2021-11-30 19:25:37 -0800

    Added comments to users and tasks files. Created tests for each function.

--- backend/tasks_sheet_backend.py
+++ backend/tasks_sheet_backend.py
@@ -2,18 +2,18 @@ import gspread as gs
 
 
 class Tasks:
-    def __init__(self):
+    def __init__(self):     # Initiates task object through gspread authentication
         self.gc = gs.service_account(filename="test_bot.json")
-        self.database_file = self.gc.open_by_url('https://docs.google.com/spreadsheets/d/1L-x43qhRBlNMnydyp'
-                                                 '-MZkqsXVXq4DhvvC9JsyjYziBs/edit#gid=73952533')
-        self.tasks = self.database_file.worksheet('database')
-        self.header = self.tasks.row_values(1)[:7]
-        self.last = self.tasks.find("LAST", in_column=1)
+        self.database_file = self.gc.open_by_url('https://docs.google.com/spreadsheets/d'
+                                                 '/13rNCLMxFNuuAJTTg9TeBIREOrr793I1PGwssldGqizA/edit#gid=73952533')
+        self.tasks = self.database_file.worksheet('Tasks')
+        self.header = self.tasks.row_values(1)[:7]          # Instantiates header descriptions
+        self.last = self.tasks.find("LAST", in_column=1)    # Keeps track of last entry
 
-    def find_task(self, target):
+    def find_task(self, target):    # Helper function to shorten gspread find functionality
         return self.tasks.find(str(target), in_column=1)
 
-    def get_task(self, task_id):
+    def get_task(self, task_id):    # Returns individual task object if found
         task_id_cell = self.find_task(task_id)
         if task_id_cell is not None:
             target_row = self.tasks.row_values(task_id_cell.row)[:7]
@@ -21,30 +21,30 @@ class Tasks:
         else:
             print("Error: TaskID(" + task_id + ") not found.")
 
-    def get_all_tasks(self):
+    def get_all_tasks(self):    # Returns all task objects in a list
         return self.tasks.get_all_records()[:-1]
 
-    def update_task(self, task_info):
+    def update_task(self, task_info):   # Updates a task object. Overwrites existing information
         target_cell = self.find_task(task_info[0])
 
-        if target_cell is None:
+        if target_cell is None:     # If a new task, adds it to the end of the list
             self.tasks.update_cell(self.last.row + 1, 1, "LAST")
             cell_list = self.tasks.range("A" + str(self.last.row) + ":G" + str(self.last.row))
             for i in range(len(cell_list)):
                 cell_list[i].value = task_info[i]
             self.tasks.update_cells(cell_list)
             self.last = self.find_task("LAST")
-        else:
+        else:                       # Updates existing task
             cell_list = self.tasks.range("A" + str(target_cell.row) + ":G" + str(target_cell.row))
             for i in range(len(cell_list)):
                 cell_list[i].value = task_info[i]
             self.tasks.update_cells(cell_list)
 
-    def batch_update_tasks(self, task_list):
+    def batch_update_tasks(self, task_list):    # Batch update of tasks objects in a list
         for task in task_list:
             self.update_task(task)
 
-    def delete_task(self, task_id):
+    def delete_task(self, task_id):     # Deletes a task by deleting a row
         task_cell = self.find_task(task_id)
         if task_cell is not None:
             self.tasks.delete_rows(task_cell.row)
@@ -53,13 +53,14 @@ class Tasks:
 
 
 test = Tasks()
-# print(test.get_task("1"))
-# test.get_task("101")
-# print(test.get_all_tasks())
-test.delete_task(6)
+print(test.get_task("1"))
+print("\n")
+print(test.get_all_tasks())
+print("\n")
+update_info = [6, "O'Connor", "207", "11/30/21", "Axel", "Payload Info", "active"]
+test.update_task(update_info)
+batch_update_info = [[3, "Casa", "343", "11/30/21", "Test", "Payload Info", "unassigned"],
+                     [7, "O'Connor", "207", "11/30/21", "Axel", "Payload Info", "active"],
+                     [8, "O'Connor", "207", "11/30/21", "Axel", "Payload Info", "active"]]
+test.batch_update_tasks(batch_update_info)
 test.delete_task(7)
-test.delete_task(8)
-# new_info = [[6, "O'Connor", "207", "11/30/21", "Axel", "Payload Info", "active"],
-#             [7, "O'Connor", "207", "11/30/21", "Axel", "Payload Info", "active"],
-#             [8, "O'Connor", "207", "11/30/21", "Axel", "Payload Info", "active"]]
-# test.batch_update_tasks(new_info)
--- backend/users_sheet_backend.py
+++ backend/users_sheet_backend.py
@@ -2,27 +2,27 @@ import gspread as gs
 
 
 class Users:
-    def __init__(self):
+    def __init__(self):     # Initiates user object through gspread authentication
         self.gc = gs.service_account(filename="test_bot.json")
-        self.database_file = self.gc.open_by_url('https://docs.google.com/spreadsheets/d/1L-x43qhRBlNMnydyp'
-                                                 '-MZkqsXVXq4DhvvC9JsyjYziBs/edit#gid=73952533')
+        self.database_file = self.gc.open_by_url('https://docs.google.com/spreadsheets/d'
+                                                 '/13rNCLMxFNuuAJTTg9TeBIREOrr793I1PGwssldGqizA/edit#gid=73952533')
         self.users = self.database_file.worksheet("Users")
         self.operator_count = len(self.users.col_values(1)) - 1
         self.admin_count = len(self.users.col_values(2)) - 1
 
-    def get_operators(self):
+    def get_operators(self):    # Returns a list of operators
         return self.users.col_values(1)[1:-1]
 
-    def get_admins(self):
+    def get_admins(self):       # Returns a list of admins
         return self.users.col_values(2)[1:-1]
 
-    def search_operators(self, target):
+    def search_operators(self, target):     # Confirms operator membership of target
         return target in self.get_operators()
 
-    def search_admins(self, target):
+    def search_admins(self, target):        # Confirms admin membership of target
         return target in self.get_admins()
 
-    def add_operator(self, new_operator):
+    def add_operator(self, new_operator):   # Adds an operator
         if not self.search_operators(new_operator):
             last = self.users.find("LAST", in_column=1)
             self.users.update_cell(last.row + 1, last.col, "LAST")
@@ -30,7 +30,7 @@ class Users:
         else:
             print("Error: " + new_operator + " is already an operator.")
 
-    def add_admin(self, new_admin):
+    def add_admin(self, new_admin):     # Adds an admin
         if not self.search_admins(new_admin):
             last = self.users.find("LAST", in_column=2)
             self.users.update_cell(last.row + 1, last.col, "LAST")
@@ -38,28 +38,26 @@ class Users:
         else:
             print("Error: " + new_admin + " is already an admin.")
 
-    def remove_operator(self, target):
+    def remove_operator(self, target):      # Removes operator
         if self.search_operators(target):
             last_pos = self.users.find("LAST", in_column=1).row + 1
             cell_list = self.users.range('A2:A' + str(last_pos))
             operators = self.users.col_values(1)[1:]
             operators.remove(target)
             operators += ["", ""]
-            print(cell_list)
             for i in range(len(cell_list)):
                 cell_list[i].value = operators[i]
             self.users.update_cells(cell_list)
         else:
             print("Error: " + target + " is not an operator.")
 
-    def remove_admin(self, target):
+    def remove_admin(self, target):     # Removes admin
         if self.search_admins(target):
             last_pos = self.users.find("LAST", in_column=2).row + 1
             cell_list = self.users.range('B2:B' + str(last_pos))
             admins = self.users.col_values(2)[1:]
             admins.remove(target)
             admins += ["", ""]
-            print(cell_list)
             for i in range(len(cell_list)):
                 cell_list[i].value = admins[i]
             self.users.update_cells(cell_list)
@@ -70,10 +68,11 @@ class Users:
 test = Users()
 print(test.get_operators())
 print(test.get_admins())
+test.add_admin("axel@scu.edu")
 print(test.search_admins("axel@scu.edu"))
 print(test.search_operators("test@scu.edu"))
 print(test.search_admins("dummy"))
-test.add_operator("Random")
-test.remove_operator("Random")
-test.add_admin("Test Admin")
-test.remove_admin("Test Admin")
+test.add_admin("Test")
+test.remove_admin("Test")
+test.add_operator("Test")
+test.remove_operator("Test")
